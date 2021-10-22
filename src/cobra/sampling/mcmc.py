1a090b9c35c1bca20df7272bb73de58725249860
schenkch <22678899+schenkch@users.noreply.github.com>
2021-10-22 14:40:08 +0200

    Update mcmc.py

--- src/cobra/sampling/mcmc.py
+++ src/cobra/sampling/mcmc.py
@@ -138,7 +138,7 @@ class MCMCACHRSampler(HRSampler):
             while counter<=nmax:
                 try:
                     self.testprev = self.prev.copy()
-                    if self.validate(self.testprev)!=v:
+                    if self.validate(self.testprev)!='v':
                         self.prev = savePrev
                         self.prev = step(self, self.prev, delta)
                         print('searching new valid sample')
