6c2dfa30fac5826c93afdfe0e8b77543d77fe05e
schenkch <22678899+schenkch@users.noreply.github.com>
2021-10-25 13:20:00 +0200

    Update mcmc.py

--- src/cobra/sampling/mcmc.py
+++ src/cobra/sampling/mcmc.py
@@ -139,14 +139,14 @@ class MCMCACHRSampler(HRSampler):
             counter = 0
             test = self.prev.copy()#numpy.ndarray
             self.testprev = np.subtract(test[0::2], test[1::2])
-            print('current sample', self.testprev)
+            #print('current sample', self.testprev)
             #print('validatecheck in progress')
             #print(self.testprev)
             print(np.transpose(self.testprev.shape))
-            print('check sample:', self.validate(np.transpose(self.testprev)))
+            #print('check sample:', self.validate(np.transpose(self.testprev)))
             #print(type(self.validate(self.testprev)))
-            if 'v' in str(self.validate(np.transpose(self.testprev))):
-                print('valid')
+            #if 'v' in str(self.validate(np.transpose(self.testprev))):
+            #    print('valid')
             while counter<=nmax and 'v' not in str(self.validate(np.transpose(self.testprev))): #input have to be netsamples and in form samples x reactions
                 if counter==nmax:
                     print('Tried to find valid sample', nmax, 'times without success')
