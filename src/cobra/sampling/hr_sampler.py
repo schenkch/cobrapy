22862ee7f9c46cff394a98e1039f984d290ef5ca
schenkch <22678899+schenkch@users.noreply.github.com>
2021-10-27 13:06:08 +0200

    Update hr_sampler.py

--- src/cobra/sampling/hr_sampler.py
+++ src/cobra/sampling/hr_sampler.py
@@ -508,23 +508,23 @@ class HRSampler(object):
 
         """
 
-        # introduce new parameter feas_tol for equality constraints, such that it can be user-defined and for MCMCACHRSampler class has new default value but just for validate function
-        if feas_tol is not None and feas_tol!=1e-7:
-            self.val_feasibility_tol = feas_tol  # if user provided set to user tolerance
-            print('Different feasibility tolerance for validate function was provided, i.e.', feas_tol)
-        elif feas_tol==1e-7:
-            self.val_feasibility_tol = feas_tol
-        else:
-            self.val_feasibility_tol = 1e-6  # else set to 1e-6 instead of model.tolerance=1e-7 from cobrapy (instead of self.feasibility_tol)
+        ## introduce new parameter feas_tol for equality constraints, such that it can be user-defined and for MCMCACHRSampler class has new default value but just for validate function
+        #if feas_tol is not None and feas_tol!=1e-7:
+            #self.val_feasibility_tol = feas_tol  # if user provided set to user tolerance
+            #print('Different feasibility tolerance for validate function was provided, i.e.', feas_tol)
+        #elif feas_tol==1e-7:
+            #self.val_feasibility_tol = feas_tol
+        #else:
+            #self.val_feasibility_tol = 1e-6  # else set to 1e-6 instead of model.tolerance=1e-7 from cobrapy (instead of self.feasibility_tol)
 
-        # introduce new parameter bounds_tol for inequality constraints, such that it can be user-defined and for MCMCACHRSampler class has new default value but just for validate function
-        if bounds_tol is not None and bounds_tol!=1e-7:
-            self.val_bounds_tol = bounds_tol  # if user provided set to user tolerance
-            print('Different bounds tolerance for validate function was provided, i.e.', bounds_tol)
-        elif bounds_tol==1e-7:
-            self.val_bounds_tol = bounds_tol
-        else:
-            self.val_bounds_tol = 1e-6 # else set to 1e-6 instead of model.tolerance=1e-7 from cobrapy (instead of self.bounds_tol)
+        ## introduce new parameter bounds_tol for inequality constraints, such that it can be user-defined and for MCMCACHRSampler class has new default value but just for validate function
+        #if bounds_tol is not None and bounds_tol!=1e-7:
+            #self.val_bounds_tol = bounds_tol  # if user provided set to user tolerance
+            #print('Different bounds tolerance for validate function was provided, i.e.', bounds_tol)
+        #elif bounds_tol==1e-7:
+            #self.val_bounds_tol = bounds_tol
+        #else:
+            #self.val_bounds_tol = 1e-6 # else set to 1e-6 instead of model.tolerance=1e-7 from cobrapy (instead of self.bounds_tol)
 
         samples = np.atleast_2d(samples)
         prob = self.problem
