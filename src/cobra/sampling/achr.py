2dd5c3f71526ef6fcd054fda1978df6267ce9277
schenkch <22678899+schenkch@users.noreply.github.com>
2021-10-27 14:13:48 +0200

    Update achr.py

--- src/cobra/sampling/achr.py
+++ src/cobra/sampling/achr.py
@@ -94,7 +94,7 @@ class ACHRSampler(HRSampler):
     def __init__(self, model, thinning=100, nproj=None, seed=None):
         """Initialize a new ACHRSampler."""
         super(ACHRSampler, self).__init__(model, thinning, nproj=nproj, seed=seed)
-        self.generate_fva_warmup(includeReversible=True)
+        self.generate_fva_warmup()
         self.prev = self.center = self.warmup.mean(axis=0)
         np.random.seed(self._seed)
 
