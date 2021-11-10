683d905fa075fd9db5207ddf6c98105381f8dfd0
schenkch <22678899+schenkch@users.noreply.github.com>
2021-11-10 11:52:31 +0100

    Update hr_sampler.py

--- src/cobra/sampling/hr_sampler.py
+++ src/cobra/sampling/hr_sampler.py
@@ -575,11 +575,11 @@ class HRSampler(object):
             #consts = consts.reshape((consts.shape[0],))
             #print(prob.bounds[0,].shape)
             #try:
-            lb_error = np.minimum(lb_error, (consts.transpose() - prob.bounds[0,]).transpose().min(axis=1))
+            lb_error = np.minimum(lb_error, (consts - prob.bounds[0,][:, np.newaxis]).min(axis=1))
             #except IndexError:
                 #lb_error = np.minimum(lb_error, (consts - prob.bounds[0,]).min())
             #try:
-            ub_error = np.minimum(ub_error, (prob.bounds[1,].transpose() - consts).transpose().min(axis=1))
+            ub_error = np.minimum(ub_error, (prob.bounds[1,][:, np.newaxis] - consts).min(axis=1))
             #except IndexError:
                 #ub_error = np.minimum(ub_error, (prob.bounds[1,] - consts).min())
 
