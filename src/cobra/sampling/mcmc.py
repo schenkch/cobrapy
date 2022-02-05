23b479ba21919ec3dbba3c2e9b06ab9c50fac13a
Christina Schenk <22678899+schenkch@users.noreply.github.com>
2022-02-05 15:48:03 +0100

    Update mcmc.py

--- src/cobra/sampling/mcmc.py
+++ src/cobra/sampling/mcmc.py
@@ -122,7 +122,7 @@ class MCMCACHRSampler(HRSampler):
     def __single_iteration(self, lockCenter=False, validatecheck=False):
         """If lockCenter, do not update the center."""
 
-        nmax = 30 #tries to find new valid sample maximum 10 times for each situation
+        nmax = 10 #tries to find new valid sample maximum 10 times for each situation
 
         pi = np.random.randint(self.n_warmup)
 
